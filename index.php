e66dd97ef2b19244013b0848b0e9a70015a097f5
iAlmohaimeed <user@Users-iMac.local>
2019-07-14 10:44:42 +0300

    Add uplaoding funtion

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
 // Hold HTTP request type
 $request_method = $_SERVER['REQUEST_METHOD'];
 switch($request_method){
@@ -17,6 +23,7 @@ function doGet(){
     echo "Get method";
 }
 function doPost(){
+    uploadToBlob();
     echo "Post method";
 }
 /* This function is responsible for fetching the data from the blob storage.
@@ -24,6 +31,13 @@ function doPost(){
    @return 
 */
 function fetchBlob(){
-    
+    echo "Fetching ... ";
+}
+/* This function is responsible for fetching the data from the blob storage.
+   @param:
+   @return 
+*/
+function uploadToBlob(){
+    echo "uploading ... ";
 }
 ?>
