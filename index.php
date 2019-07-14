661c1f218750956560ab9f42cfffc9a04f35be09
iAlmohaimeed <user@Users-iMac.local>
2019-07-14 12:53:14 +0300

    Add a connect() and a listing function

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+require_once __DIR__.'/vendor/autoload.php';
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use WindowsAzure\Common\ServicesBuilder;
 
 // Hold HTTP request type
 $request_method = $_SERVER['REQUEST_METHOD'];
@@ -31,32 +32,44 @@ function doPost(){
    @return 
 */
 function fetchBlob(){
-    echo "Fetching ... ";
+    echo "Connecting ...\n";    
+    // Get a connection instance. 
+    $blobClient = connect();
+    // Create a new ListBlobOption instance.
+    $listBlobsOptions = new ListBlobsOptions();
+    // Specify the name of the container.
+    $containerName = "test";
+    echo "Fetching ... \n";
+    echo "These are the blobs present in the container: \n";
+    do{
+        // Get the list of all the blobs on the specified container.
+        $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+        // iterates through the list of blobs.
+        foreach ($result->getBlobs() as $blob)
+        {
+            echo $blob->getName().": ".$blob->getUrl()."\n";
+        }
+        
+        $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+    } while($result->getContinuationToken());
+    echo "Done.";
 }
-/* This function is responsible for fetching the data from the blob storage.
+/* This function is responsible for connecting to the blob storage account
    @param:
-   @return 
+        AccountName: <String> Can be found on the access option on you Azure->Storage-accounts->access-keys.
+        AccountKey: <String> Can be found on the Access keys option on your Azure->Storage-accounts->access-keys.
+   @return:
+        blobClient: A Client connection instance. 
 */
-function uploadToBlob(){
+function connect(){
     # Setup a specific instance of an Azure::Storage::Client
     $connectionString = "DefaultEndpointsProtocol=https;AccountName=ibrahim20;AccountKey=wMCFPXsA/klI6OGSrmdc1jgFIAJa3bRyD9mhtH31fS9OLCnlhGL8Er/TSc9uKrMMt1GYinFBkIuC5lP2krt/IA==";
-    
     // Create blob client.
     $blobClient = BlobRestProxy::createBlobService($connectionString);
     
-    $myfile = fopen("HelloWorld.txt", "w") or die("Unable to open file!");
-    fclose($myfile);
-
-    # Upload file as a block blob
-    echo "Uploading BlockBlob: ".PHP_EOL;
-    echo $fileToUpload;
-    echo "<br />";
-    
-    $content = fopen($fileToUpload, "r");
-
-    //Upload blob
-    $containerName = "test";
-    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    return $blobClient;
+}
+function uploadToBlob(){
     echo "uploading ... ";
 }
 ?>
