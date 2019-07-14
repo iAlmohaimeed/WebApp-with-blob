b2a29e37b5795d7a3762a328117c81ce95def977
iAlmohaimeed <user@Users-iMac.local>
2019-07-14 13:13:41 +0300

    fix

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
+require_once __DIR__.'/vendor/autoload.php';
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use WindowsAzure\Common\ServicesBuilder;
 
 // Hold HTTP request type
 $request_method = $_SERVER['REQUEST_METHOD'];
