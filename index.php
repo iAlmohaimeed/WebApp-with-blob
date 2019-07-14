086d1b1c5e95cad33f55c280ee9fd0758f9b7653
iAlmohaimeed <user@Users-iMac.local>
2019-07-14 12:19:34 +0300

    Add upload func.

--- index.php
+++ index.php
@@ -38,6 +38,24 @@ function fetchBlob(){
    @return 
 */
 function uploadToBlob(){
+    # Setup a specific instance of an Azure::Storage::Client
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName=ibrahim20;AccountKey=wMCFPXsA/klI6OGSrmdc1jgFIAJa3bRyD9mhtH31fS9OLCnlhGL8Er/TSc9uKrMMt1GYinFBkIuC5lP2krt/IA==";
+    
+    // Create blob client.
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
+    
+    $myfile = fopen("HelloWorld.txt", "w") or die("Unable to open file!");
+    fclose($myfile);
+
+    # Upload file as a block blob
+    echo "Uploading BlockBlob: ".PHP_EOL;
+    echo $fileToUpload;
+    echo "<br />";
+    
+    $content = fopen($fileToUpload, "r");
+
+    //Upload blob
+    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
     echo "uploading ... ";
 }
 ?>
