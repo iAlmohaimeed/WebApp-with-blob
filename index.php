1a353f8b2b82649c9e0d5dfe4fb33475c7cf9aa1
iAlmohaimeed <user@Users-iMac.local>
2019-07-14 14:34:48 +0300

    Fix issue

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
+require_once __DIR__.'/vendor/autoload.php';
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use WindowsAzure\Common\ServicesBuilder;
 
 // Hold HTTP request type
 $request_method = $_SERVER['REQUEST_METHOD'];
@@ -13,7 +20,7 @@ switch($request_method){
         break;
 }
 function doGet(){
-   // fetchBlob();
+    fetchBlob();
     echo "Get method";
 }
 function doPost(){
@@ -25,7 +32,26 @@ function doPost(){
    @return 
 */
 function fetchBlob(){
-    
+    echo "Connecting ...\n";    
+    // Get a connection instance. 
+    $blobClient = connect();
+    // Create a new ListBlobOption instance.
+    $listBlobsOptions = new ListBlobsOptions();
+    // Specify the name of the container.
+    $containerName = "test";
+    echo "Fetching ... \n";
+    echo "These are the blobs present in the container: \n";
+    do{
+        // Get the list of all the blobs on the specified container.
+        $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+        // iterates through the list of blobs.
+        foreach ($result->getBlobs() as $blob)
+        {
+            echo $blob->getName().": ".$blob->getUrl()."\n";
+        }
+        
+        $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+    } while($result->getContinuationToken());
     echo "Done.\n";
 }
 /* This function is responsible for connecting to the blob storage account
@@ -35,7 +61,14 @@ function fetchBlob(){
    @return:
         blobClient: A Client connection instance. 
 */
-
+function connect(){
+    # Setup a specific instance of an Azure::Storage::Client
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName=ibrahim20;AccountKey=wMCFPXsA/klI6OGSrmdc1jgFIAJa3bRyD9mhtH31fS9OLCnlhGL8Er/TSc9uKrMMt1GYinFBkIuC5lP2krt/IA==";
+    // Create blob client.
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
+    
+    return $blobClient;
+}
 function uploadToBlob(){
     echo "uploading ... ";
 }
